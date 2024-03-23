cedca5c36665579ce20c463f7073782ca48b0ca6
Rafael <krafaelcoelho@gmail.com>
2024-03-23 01:50:02 -0300

    page icon

--- script.js
+++ script.js
@@ -10,13 +10,17 @@ function toggleMode (){
     html.classList.toggle('light') 
 
     const img = document.querySelector('#profile img')
+    var iconLink = document.querySelector('link[rel="icon"]');
+
 
     if (html.classList.contains('light')){
         img.setAttribute("src", "./assets/Avatar.png")
         img.setAttribute("alt", "foto Michael Scott light mode")
+        iconLink.setAttribute("href","./assets/sun.svg")
     } else {
         img.setAttribute("src", "./assets/Avatar-dark.png")
         img.setAttribute("alt", "foto de perfil dark mode")
+        iconLink.setAttribute("href","https://cdn-icons-png.flaticon.com/512/1229/1229467.png")
     }
 
 
